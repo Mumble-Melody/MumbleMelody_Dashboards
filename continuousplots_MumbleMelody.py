8d32092cf9cc3d0fb834b20f976d7e54763a838f
alishakodibagkar <akodibagkar@gmail.com>
2022-09-08 14:34:25 -0400

    fix variable name

--- continuousplots_MumbleMelody.py
+++ continuousplots_MumbleMelody.py
@@ -32,7 +32,7 @@ import json
 
 with open('mumble-melody-longitudinal-firebase-adminsdk-34x0r-52f98ad6f0.json', 'r+') as f:
     firebase_json = json.load(f)
-    firebase_json['private_key_id'] = str(FIREBASE_PRIVATE_KEY)
+    firebase_json['private_key_id'] = str(FIREBASE_PRIVATE_KEY_ID)
     firebase_json['private_key'] = "-----BEGIN PRIVATE KEY-----\n" + str(FIREBASE_PRIVATE_KEY) + "=\n-----END PRIVATE KEY-----\n"
 
 cred = credentials.Certificate(firebase_json)
